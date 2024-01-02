7f250091a90d880cb1fa889b0af83e820604e1e8
praveen <praveen.mangal@shipway.com>
2024-01-02 23:08:57 +0530

    Added dynamic making of grid by user

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-let container = document.getElementById('container');
-
-for (let i = 0; i < 16; i++) {
-	let row = document.createElement('div');
-	for (let j = 0; j < 16; j++) {
-		let div = document.createElement('div');
-		div.classList.add('box');
-		row.appendChild(div);
+let btn = document.querySelector('button');
+btn.addEventListener('click', function () {
+	let gridLength = prompt('Enter no. of squares(less than 100)', '');
+	if (gridLength === null || gridLength === '') {
+		return;
 	}
-	container.appendChild(row);
-}
+	if (gridLength > 100) {
+		return;
+	}
+	
+	let container = document.getElementById('container');
+	container.innerHTML = '';
+	for (let i = 0; i < gridLength; i++) {
+		let row = document.createElement('div');
+		for (let j = 0; j < gridLength; j++) {
+			let div = document.createElement('div');
+			div.classList.add('box');
+			row.appendChild(div);
+		}
+		container.appendChild(row);
+	}
 
-let boxes = document.getElementsByClassName('box');
-Array.from(boxes).forEach(box => box.addEventListener('mouseover', function () {
-	this.style.backgroundColor = 'black';
-}));
+	let boxes = document.getElementsByClassName('box');
+	Array.from(boxes).forEach(box => box.addEventListener('mouseover', function () {
+		this.style.backgroundColor = 'black';
+	}));
+});
