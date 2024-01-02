2c31d9260d9362bde025052752fb6025d5aeafff
praveen <praveen.mangal@shipway.com>
2024-01-02 23:34:27 +0530

    Added on variable grid, container will have fixed width, boxed will adjust

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 let btn = document.querySelector('button');
 btn.addEventListener('click', function () {
-	let gridLength = prompt('Enter no. of squares(less than 100)', '');
-	if (gridLength === null || gridLength === '') {
+	let gridLength = parseInt(prompt('Enter no. of squares(less than 100)', ''));
+	if (isNaN(gridLength)) {
 		return;
 	}
 	if (gridLength > 100) {
 		return;
 	}
-	
+
 	let container = document.getElementById('container');
+	let containerWidth = parseInt(window.getComputedStyle(container).width);
+
+	let boxWidth = containerWidth / gridLength;
 	container.innerHTML = '';
+
 	for (let i = 0; i < gridLength; i++) {
 		let row = document.createElement('div');
 		for (let j = 0; j < gridLength; j++) {
 			let div = document.createElement('div');
 			div.classList.add('box');
+			div.style.cssText = `width : ${boxWidth}px; 
+			height : ${boxWidth}px;`;
 			row.appendChild(div);
 		}
 		container.appendChild(row);
